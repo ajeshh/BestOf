ed30de8f392487e810a64b1dd5d5d342b547ae9c
Ajesh Shah <ajeshshah@gmail.com>
2015-02-17 14:59:10 -0800

    installed omniauth and dotenv gems

---
+++ db/schema.rb
@@ -0,0 +1,83 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150217223634) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bests", force: :cascade do |t|
+    t.boolean  "restaurant"
+    t.boolean  "neighborhood"
+    t.boolean  "city"
+    t.boolean  "ever"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "checkins", force: :cascade do |t|
+    t.integer  "foursquare_check_id"
+    t.integer  "user_id"
+    t.integer  "venue_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+  create_table "itembests", force: :cascade do |t|
+    t.integer  "item_id"
+    t.integer  "best_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "userbests", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "best_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.integer  "foursquare_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "venuebests", force: :cascade do |t|
+    t.integer  "venue_id"
+    t.integer  "best_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venueitems", force: :cascade do |t|
+    t.integer  "venue_id"
+    t.integer  "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.integer  "foursquare_venue_id"
+    t.string   "venue_name"
+    t.string   "venue_type"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
+
+end
