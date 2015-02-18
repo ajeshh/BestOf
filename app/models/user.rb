afa06d7d9fad728bd9086853d4cd3ed94cbcfb9f
Ajesh Shah <ajeshshah@gmail.com>
2015-02-17 16:47:53 -0800

    omniauth foursquare

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
 	has_many :userbests
 	has_many :venues, :through => :checkins
 	has_many :bests, :through => :userbests
+
+	# def self.from_omniauth(auth)
+	#     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+	# end
+	  
+	# def self.create_from_omniauth(auth)
+	#     create! do |user|
+	#       user.provider = auth["provider"]
+	#       user.uid = auth["uid"]
+	#     end
+	# end
+
+	def self.from_omniauth(auth)
+	    where(uid: auth.uid).first_or_initialize.tap do |user|
+	      user.provider = auth.provider
+	      user.uid = auth.uid
+	    end
+  	end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,8 @@
+OmniAuth.config.logger = Rails.logger
+
 Rails.application.config.middleware.use OmniAuth::Builder do
+  puts ENV['consumer_key']
+  puts ENV['consumer_secret']
   provider :foursquare, ENV['consumer_key'], ENV['consumer_secret']
 
 end
